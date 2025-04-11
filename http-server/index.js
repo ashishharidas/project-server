73b171926fa6374592df86340a265be49f218db1
ashishharidas <ashish101111@gmail.com>
2025-04-12 00:14:41 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const fs = require("fs");
+const minimist = require("minimist");
 
-// Read only the necessary HTML files
+// Parse command line args
+const args = minimist(process.argv.slice(2));
+const PORT = args.port || 3001;
+
+// Load HTML files
 const homeContent = fs.readFileSync("home.html", "utf-8");
 const projectContent = fs.readFileSync("project.html", "utf-8");
+const registrationContent = fs.readFileSync("registration.html", "utf-8");
 
 http
   .createServer((request, response) => {
@@ -17,11 +23,14 @@ http
       case "/project":
         response.end(projectContent);
         break;
+      case "/registration":
+        response.end(registrationContent);
+        break;
       default:
         response.writeHead(404, { "Content-Type": "text/html" });
         response.end("<h1>404 - Page Not Found</h1>");
     }
   })
-  .listen(3001, () => {
-    console.log("Server running at http://localhost:3001");
+  .listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
   });
